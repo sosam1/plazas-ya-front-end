64dd4cf2acb9c4fa706b21f5f51b4fcf29baaf89
sosam1 <sosam1763@gmail.com>
2023-12-14 17:22:18 -0300

    agregando datos a cada plaza

--- js/main.js
+++ js/main.js
@@ -1,74 +1,103 @@
-const URL_PLAZAS = "http://127.0.0.1:8000/api/plaza"
 
-function initMap() {
-    var uruguayCoords = { lat: -32.522779, lng: -55.765835 };
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const URL_PLAZAS = "http://127.0.0.1:8000/api/plaza"
 
-    var mapOptions = {
-        center: uruguayCoords,
-        zoom: 6,
-    };
+    function initMap() {
+        var uruguayCoords = { lat: -32.522779, lng: -55.765835 };
 
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        var mapOptions = {
+            center: uruguayCoords,
+            zoom: 6,
+        };
 
-    // Autocompletar para el campo de entrada
-    var input = document.getElementById('address-input');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-
-    // Enlazar el autocompletar con el mapa
-    autocomplete.bindTo('bounds', map);
-
-
-    var centerButton = document.getElementById('centerButton');
-    centerButton.addEventListener('click', function () {
-        // Obtener la dirección del campo de entrada
-        var address = input.value;
-
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode({ 'address': address }, function (results, status) {
-            if (status === 'OK') {
-                // Obtener las coordenadas de la primera coincidencia
-                var location = results[0].geometry.location;
-
-                // Crear un marcador en la ubicación de la dirección
-                var marker = new google.maps.Marker({
-                    position: location,
-                    map: map,
-                    title: 'Destino'
-                });
-
-                // Centrar el mapa en la ubicación del nuevo marcador
-                map.setCenter(location);
-
-                map.setZoom(15);
-            } else {
-                alert('Geocodificación inversa fallida debido a: ' + status);
-            }
-        });
-    });
-}
-
-function obtenerTodasLasPlazas(){
-
-    fetch(URL_PLAZAS)
-
-    .then(response => {
-        if (!response.ok) {
-          throw new Error('Error de red o servidor');
-        }
-        return response.json();
-      })
-
-      .then(plazas => {
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
         
 
-        console.log(plazas);
+    }
+
+    initMap()
+
+    function obtenerTodasLasPlazas(){
+
+        fetch(URL_PLAZAS)
+
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Error de red o servidor');
+            }
+            return response.json();
+        })
+
+        .then(plazas => {
+            
+
+            console.log(plazas);
+
+            let container_plazas = document.getElementById("plazas_container")
+
+            plazas.forEach(plaza => {
+
+                const puntuacionRedondeada = Math.floor(plaza.valoracion);
+                const puntuacionDecimal = plaza.valoracion - puntuacionRedondeada;
+
+                // Genera las estrellas en HTML
+                let estrellasHTML = '';
+                for (let i = 0; i < 5; i++) {
+                    if (i < puntuacionRedondeada) {
+                        estrellasHTML += '<i class="fa-solid fa-star"></i>';
+                    } else if (i === puntuacionRedondeada && puntuacionDecimal > 0) {
+                        // Agrega una estrella parcialmente llena
+                        const opacidad = puntuacionDecimal;
+                        estrellasHTML += `<i class="fa-solid fa-star" style="color: #ffd700; opacity: ${opacidad};"></i>`;
+                    } else {
+                        estrellasHTML += '<i class="fa-regular fa-star"></i>';
+                    }
+                }
+                
+                container_plazas.innerHTML += `
+                
+                <div class="plaza_objeto" onclick="buscarPlazaEnMapa(${plaza.latitud}, ${plaza.longitud})">
+
+                <h2 class="plaza_titulo">${plaza.nombre_plaza}</h2>
+                
+                <p><i class="fa-solid fa-location-dot ubicacion_icono"></i>&nbsp;${plaza.direccion}</p>
+                <br>
+                <p>${estrellasHTML} Calificación: ${plaza.valoracion} - ${plaza.cantidad_resenas} opiniones</p>
+
+                </div>
+                
+                `
+
+            });
 
 
-      })
+        })
 
-      .catch(error => {
-        console.error('Hubo un problema con la petición Fetch:', error);
-      });
-}
+        .catch(error => {
+            console.error('Hubo un problema con la petición Fetch:', error);
+        });
+    }
 
-obtenerTodasLasPlazas()
+    obtenerTodasLasPlazas()
+
+    window.buscarPlazaEnMapa = function(latitud, longitud) {
+        
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: latitud, lng: longitud },
+            zoom: 15
+        });
+        
+        var marker = new google.maps.Marker({
+            position: { lat: latitud, lng: longitud },
+            map: map,
+            title: 'Ubicación de la plaza'
+        });
+
+        
+
+    };
+
+});
+
+
