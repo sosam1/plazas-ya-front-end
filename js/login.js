c962ee54f08d4d0f9e28d039063c980dd142f4ee
sosam1 <sosam1763@gmail.com>
2024-01-05 14:29:57 -0300

    registro css

--- js/login.js
+++ js/login.js
@@ -1,20 +1,17 @@
-URL_API_AUTENTICACION = "http://127.0.0.1:8001/api/usuarios/registrar"
+URL_API_AUTENTICACION = "http://127.0.0.1:8001/api/usuarios/login"
 
 let userInput = document.getElementById("userInput")
-let emailInput = document.getElementById("emailInput")
 let passInput = document.getElementById("passInput")
-let pass2Input = document.getElementById("pass2Input")
 
-function crearUsuario(){
+
+function login(){
 
     jQuery.ajax({
-        url: 'http://127.0.0.1:8001/api/usuarios/registrar',
+        url: 'http://127.0.0.1:8001/api/usuarios/login',
         type: 'POST',
         data: {
             'username': userInput.value,
-            'email': emailInput.value,
             'password': passInput.value,
-            'password_confirmation': pass2Input.value
         },
 
         success: function(data) {
